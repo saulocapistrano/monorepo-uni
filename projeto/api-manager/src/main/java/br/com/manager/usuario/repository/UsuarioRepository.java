d7924cc48a26ddba4ddc0bd464ea4a3bebe67c9c
saulocapistrano <saulocapistrano@gmail.com>
2025-07-07 09:34:02 -0300

    feat(repository): cria interface UsuarioRepository estendendo JpaRepository

---
+++ projeto/api-manager/src/main/java/br/com/manager/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.manager.usuario.repository;
+
+import br.com.manager.usuario.model.Usuario;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UsuarioRepository implements PanacheRepository<Usuario> {
+}
