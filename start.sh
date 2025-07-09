2adb075d4142bc13fff6c161d5712a18e2e37470
saulocapistrano <saulocapistrano@gmail.com>
2025-07-08 22:08:57 -0300

    chore(start.sh): adiciona build automático do frontend antes de subir os containers

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ set -a
 source .env.secret
 set +a
 
+echo "Realizando build do frontend (ui-manager)..."
+cd projeto/ui-manager
+npm install
+npm ng build --configuration=production
+cd ../../
+
 echo "Iniciando infraestrutura com Docker Compose..."
 docker compose -f docker/docker-compose.yml --env-file .env up -d --build
-
